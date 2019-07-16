4b80b551c54ce2d57e763d967853a50ef1da58e2
Jess Rezac <jess.rezac@gmail.com>
2019-07-16 16:42:25 +0000

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'Friendly', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 8, 'Female', 'Black', 'Mean as Heck', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 13, 'Female', 'Brown', 'Very Nice', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Seargent Brown', 2, 'Male', 'White', 'Friendly', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 28, 'Female', 'Brown', 'Motherly', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 12, 'Male', 'Black', 'Grumpy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 18, 'Female', 'White', 'Sassy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, 'Male', 'White', 'Mean as Heck', true);
