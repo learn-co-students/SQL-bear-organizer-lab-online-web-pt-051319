57ab41f405d525f3eac1b05be5f0fd0ebfee703f
Santiago Sanchez <chagostwo@yahoo.com>
2019-07-19 03:00:09 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Wendy', 3, 'male','grey','chill','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Grinch', 4, 'female','black','moody','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Melissa', 100, 'male','orange','chill','false');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Sergeant Brown', 50, 'male','purple','aggressive','false');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Tabitha', 7, 'female','black','quiet','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Rowdy', 6, 'male','brown','loud','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ( NULL, 3, 'female','red','energetic','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Mr. Chocolate', 9, 'male','grey','lazy','true');
